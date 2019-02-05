07d108154f357d159282545c41b0be5d36335d30
RemiFrontiere <remi.frontiere@gmail.com>
2019-02-05 01:15:18 +0100

    add stargate and responsive

--- src/app/modules/main/home/home.component.ts
+++ src/app/modules/main/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnDestroy, ChangeDetectorRef, HostListener, Inject } from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
 
 import {TranslateService} from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'home-root',
@@ -9,25 +10,51 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponentMain implements OnDestroy {
-  public mobileQuery: MediaQueryList;
+  
+  public audio:HTMLAudioElement;
+  public isScrolled:boolean = false;
+  public stargateisTurning:boolean = false;
+  public stargateNumberOfSymbols:Array<string> = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'];
+  public stargateExtBorderNumber:[] = Array.apply(null, {length: 150}).map(Function.call, Number);
+  // public mobileQuery: MediaQueryList;
+  // private _mobileQueryListener: () => void;
 
-  private _mobileQueryListener: () => void;
+  constructor(
+    // changeDetectorRef: ChangeDetectorRef, 
+    // media: MediaMatcher, 
+    public translate: TranslateService) {
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, public translate: TranslateService) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+    // this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    // this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    // this.mobileQuery.addListener(this._mobileQueryListener);
 
     translate.setDefaultLang('fr');
     translate.use('fr');
   }
 
   ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
+    // this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
   public changeLang():void{
     this.translate.currentLang == 'fr' ? this.translate.use('en') :  this.translate.use('fr')
+  }
+
+  public rotateStargate():void{
+    this.stargateisTurning = !this.stargateisTurning;
+    if(this.stargateisTurning){
+      this.audio = new Audio();
+      this.audio.src = "assets/audio/roll.mp3";
+      this.audio.load(); 
+      this.audio.loop = true;
+      this.audio.play();
+      this.audio.volume = 0.05;
+    }
+    else{
+      if(this.audio){
+        this.audio.pause();
+      }
+    }
   }
 
   shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
